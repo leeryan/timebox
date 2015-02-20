93289716e9b77726137f1b753d57c21eb2258d6a
Lee Ryan <lee.j.ryan@icloud.com>
2015-02-20 14:53:49 +0000

    Basic archetype

---
+++ dev-automation/gruntfile.js
@@ -0,0 +1,63 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    connect: {
+      server: {
+        options: {
+          port: 8080,
+          base: '../app/dev/',
+          livereload: true
+        }
+      }
+    },
+
+    concurrent: {
+      target: {
+            tasks: ['nodemon', 'watch'],
+            options: {
+                logConcurrentOutput: true
+            }
+        }
+    },
+
+    nodemon: {
+      dev: {
+        script: 'gruntfile.js'
+      }
+    },
+
+    copy: {
+      bower: {
+      }
+    },
+
+    watch: {
+
+      markup: {
+        files: '../**/*.html',
+        options: {
+          livereload: true
+        }
+      },
+
+      scripts: '../**/*.js',
+      options: {
+        livereload: true
+      }
+
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-concurrent');
+  grunt.loadNpmTasks('grunt-nodemon');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+
+  grunt.registerTask('default', ['connect', 'concurrent']);
+  grunt.registerTask('bower', ['copy:bower']);
+
+};
